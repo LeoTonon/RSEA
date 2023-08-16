68813ded71efeaf31bbaf01f8a1859e404500e5d
PedroGitBM <116378938+PedroGitBM@users.noreply.github.com>
2023-08-16 14:39:26 -0300

    Add files via upload

---
+++ exemplo_api/src/main/java/com/example/exemplo_api/ExemploApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.exemplo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApiApplication.class, args);
+	}
+
+}
---
+++ exemplo_api/src/test/java/com/example/exemplo_api/ExemploApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exemplo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

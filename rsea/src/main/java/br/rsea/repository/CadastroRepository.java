cd79d0f66ed736f4606da039bcba76dac2dde306
Leonardo Alberto Tonon da Silva <leonardoat547@gmail.com>
2023-10-02 10:57:50 -0300

    Tabela Comunidade Funcionando parcialmente

--- rsea/src/main/java/br/rsea/repository/CadastroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.rsea.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.rsea.model.Cadastro;
-
-public interface CadastroRepository extends CrudRepository<Cadastro, Integer>{
-    
-}
